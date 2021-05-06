8e9bb290b54c57d514de1f84e29ce2fce7b671de
Harika <h.duyu@rebuy.com>
2021-05-06 20:23:42 +0200

    add create and drop queries

--- sql_queries.py
+++ sql_queries.py
@@ -1,26 +1,31 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time date, user_id int, level varchar, song_id int, artist_id int, session_id int, location varchar, user_agent varchar);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (user_id int, first_name varchar, last_name varchar, gender varchar, level varchar);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (songid int, title varchar, artist_id int, year int, duration float);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (artist_id int, name varchar, location varchar, latitude float, longitude float);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (start_time bigint, hour int, day int, week int, month int, year int, weekday int);
 """)
 
 # INSERT RECORDS
