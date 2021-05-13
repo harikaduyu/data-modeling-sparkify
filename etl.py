6a5c87645b578a9cf31bb727d0d4820278f319e8
Harika <h.duyu@rebuy.com>
2021-05-13 17:31:13 +0200

    add docstrings

--- etl.py
+++ etl.py
@@ -6,38 +6,65 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """A function that processes single song file to get song data and artist data from it and inserts these data into song and artist tables respectively. It is assumed that each file has data for only one song in it.
+    
+     Parameters
+    ----------
+    cur : psycopg2.extensions.cursor
+        A cursor which connects to the database and allows python to execute PostgreSQL commands
+    filepath : str
+       The file location of the song data which is a json file
+
+    Returns
+    -------
+    None
+    """
+
     # open song file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = 
+    song_data = df[["song_id","title","artist_id","year","duration"]].values.tolist()[0]
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = 
+    artist_data = df[["artist_id","artist_name","artist_location","artist_latitude","artist_longitude"]].values.tolist()[0]
     cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
+    """A function that processes single log file to get user data, time data and songplay data from it and inserts these data into user,time and songplay tables respectively
+    
+     Parameters
+    ----------
+    cur : psycopg2.extensions.cursor
+        A cursor which connects to the database and allows python to execute PostgreSQL commands
+    filepath : str
+       The file location of the log data which is a json file
+
+    Returns
+    -------
+    None
+    """
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df.loc[df["page"]=="NextSong"]
 
     # convert timestamp column to datetime
-    t = 
+    t =  pd.to_datetime(df["ts"], unit='ms') 
     
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_data = (df["ts"], t.dt.hour, t.dt.day, t.dt.isocalendar().week, t.dt.month, t.dt.year, t.dt.weekday )
+    column_labels = ("timestamp", "hour", "day", "week of year", "month", "year", "weekday")
+    time_df = pd.DataFrame(dict((zip(column_labels, time_data)))).drop_duplicates()
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
 
     # load user table
-    user_df = 
+    user_df = df[["userId","firstName", "lastName","gender","level"]].drop_duplicates()
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -56,11 +83,29 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data =  (index, row.ts, row.userId, row.level, songid,artistid,row.sessionId,row.location, row.userAgent )
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+    """A function that walks thorugh each file in a given filepath, and processes data using a given processor function. 
+    
+     Parameters
+    ----------
+    cur : psycopg2.extensions.cursor
+        A cursor which connects to the database and allows python to execute PostgreSQL commands
+    conn : psycopg2.extensions.connection
+        Handles connection to a PostgreSQL database instance
+    filepath : str
+       The file path which contains the corresponding json files (either log filer or song files)
+    func : function
+        The function which handles processing the file. It is either process_song_file or process_log_file.
+
+
+    Returns
+    -------
+    None
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
